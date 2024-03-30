16e24096cc2f3bb335b8ea733085ff5fe18777a9
Ricardo Trejos <cardotrejos@gmail.com>
2024-03-30 10:52:18 -0500

    feat: Refactor home component for dynamic weather locations
    The home component has been refactored to dynamically generate links based on an array of weather locations. The HTML files for the app and home components have been deleted, and their content moved directly into the TypeScript file of the home component. CommonModule was also imported to enable usage of Angular's common directives in the template.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { WeatherLocation } from '../models/weather.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  imports: [RouterModule, CommonModule],
+  template: `
+    <div>
+      <a
+        *ngFor="let location of weatherLocations"
+        [routerLink]="['/weather', location.id]"
+      >
+        {{ location.name }} Forecast
+      </a>
+    </div>
+  `,
 })
 export class HomeComponent {
-
+  weatherLocations: WeatherLocation[] = [
+    { id: 'TOP', name: 'Kansas' },
+    { id: 'LWX', name: 'District of Columbia' },
+  ];
 }
